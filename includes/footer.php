b56c4f88bb0d16c5a3c61e20542583655cbecf1f
IMxWolF <patneyash123@gmail.com>
2026-07-11 13:57:57 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,25 @@
+<footer class="footer">
+
+    <div class="footer-content">
+
+        <div class="footer-logo">
+            JD.
+        </div>
+
+        <div class="footer-social">
+
+            <a href="#"><i class="fab fa-github"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+
+        </div>
+
+        <p>Crafted and deployed with passion.</p>
+
+    </div>
+
+</footer>
+
 <script src="assets/js/main.js"></script>
 
 </body>
