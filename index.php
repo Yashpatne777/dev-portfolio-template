1f5c4160c1cda88afe96f78d30cb37e5fe0388ea
IMxWolF <patneyash123@gmail.com>
2026-07-11 12:56:48 +0530

    a

--- index.php
+++ index.php
@@ -50,5 +50,83 @@
     </div>
 
 </section>
+<section class="about" id="about">
+
+    <div class="container">
+
+        <div class="section-title">
+
+            <span>About Me</span>
+
+            <h2>Who I Am</h2>
+
+        </div>
+
+        <div class="about-wrapper">
+
+            <div class="about-image">
+
+                <img src="assets/images/profile.jpg"
+                alt="Profile">
+
+            </div>
+
+            <div class="about-content">
+
+                <h3>
+                    Frontend Developer &
+                    Web Creator
+                </h3>
+
+                <p>
+
+                    I'm a passionate web developer
+                    focused on creating modern,
+                    responsive and visually
+                    appealing websites.
+
+                </p>
+
+                <p>
+
+                    I specialize in HTML, CSS,
+                    JavaScript, PHP, WordPress,
+                    Next.js and FiveM web
+                    development.
+
+                </p>
+
+                <div class="about-info">
+
+                    <div class="info-box">
+
+                        <h4>2+</h4>
+                        <span>Years Learning</span>
+
+                    </div>
+
+                    <div class="info-box">
+
+                        <h4>10+</h4>
+                        <span>Projects Built</span>
+
+                    </div>
+
+                    <div class="info-box">
+
+                        <h4>100%</h4>
+                        <span>Dedication</span>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
 
 <?php include 'includes/footer.php'; ?>
